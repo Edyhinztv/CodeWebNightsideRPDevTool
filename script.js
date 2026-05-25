20e7aede53844ac5e72f8a8e748e880bbc6e6897
Edyhinztv <negosiosedyhinz@gmail.com>
2026-05-24 20:49:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@
 
     // IDs de Discord que tienen acceso (el ID real que Discord devuelve)
     const ALLOWED_IDS = [
-        '1357141184631279656',   // ← reemplaza con los IDs reales
+        '1357141184631279656',
     ];
     // ════════════════════════════════════════════════
 
